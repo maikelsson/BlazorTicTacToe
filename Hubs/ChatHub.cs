8ebdc4798755c30f9aec09c44850541412b8780c
mikael thornberg <mikael.thornberg@hotmail.com>
2020-10-04 12:52:52 +0300

    went back to old style for hubconnection etc. added also event that keeps track of current connections in chat room

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,4 +1,5 @@
-﻿using Blazored.SessionStorage;
+﻿
+using BlazorServerApp_Chess.Data;
 using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
@@ -16,32 +17,34 @@ namespace BlazorServerApp_Chess.Hubs
 
         public static HashSet<string> connectionIds = new HashSet<string>();
 
-        public async override Task OnConnectedAsync()
+        public override Task OnConnectedAsync()
         {
             if (!connectionIds.Contains(Context.ConnectionId))
             {
                 connectionIds.Add(Context.ConnectionId);
+                Clients.All.SendAsync("ReceiveUsersCount", connectionIds.Count);
             }
-
-            await base.OnConnectedAsync();
+            
+            return base.OnConnectedAsync();
         }
 
         public override Task OnDisconnectedAsync(Exception exception)
         {
             connectionIds.Remove(Context.ConnectionId);
+            Clients.All.SendAsync("ReceiveUsersCount", connectionIds.Count);
             return base.OnDisconnectedAsync(exception);
         }
 
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message, DateTime.Now.ToShortTimeString());
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
             
         }
 
         public async Task SendMessageToOthers(string user)
         {
             string message = $"{user} connected to chat!";
-            await Clients.Others.SendAsync("ReceiveMessage", user, message, DateTime.Now.ToShortTimeString());
+            await Clients.Others.SendAsync("ReceiveMessage", user, message);
         }
 
         public async Task<int> GetAllActiveConnections()
